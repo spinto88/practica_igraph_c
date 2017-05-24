7c1fc56a43aae7d356564def108a2750433aff3b
spinto <spinto@df.uba.ar>
2017-05-24 13:22:08 -0300

    Ningun cambio en especial

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from zipfile import ZipFile
 import os
 from dynamics import dynamics
 
-N = 400
+N = 2500
 F = 11
 
 virtual_links = N * 4
 
-steps = 50000
+steps = 1000
 
 rand.seed(123457)
 
 for conf in range(100):
 
-    for q in range(10, 500, 50):
+    for q in [30]: #range(10, 500, 50):
 
         dynamics(N, F, q, virtual_links, steps)
-           
+
+    exit()
+    """           
     myzip = ZipFile('Graphs_prueba' + str(conf) + '.zip', 'a')
     myzip.write('Graph_q' + str(q) + '_vl' + str(virtual_links) + '.graphml')
     myzip.close()
-    os.remove('Graph_q' + str(q) + '_vl' + str(virtual_links) + '.graphml')        
+    os.remove('Graph_q' + str(q) + '_vl' + str(virtual_links) + '.graphml')         """
