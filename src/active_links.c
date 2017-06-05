67948041219acf0bc23f6c856ad645b4d714fafc
spinto <spinto@df.uba.ar>
2017-06-05 14:50:07 -0300

    Bue, a confiar que ande

--- src/active_links.c
+++ src/active_links.c
@@ -26,7 +26,7 @@ int active_links(igraph_t *graph, axl_agent *agents)
 			if (i < neighbour)
 			{
 				hab = homophily(agents[i], agents[neighbour]);
-				if(hab <= (1.00/agents[i].f))
+				if(hab <= (2.00/agents[i].f))
 					hab = 0.00;
 
 				if((0.00 < hab) && (hab < 1.00))
